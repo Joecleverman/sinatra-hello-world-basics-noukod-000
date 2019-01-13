43c84273aefeaadbcfe642afe66bfae4f4468f37
Josue Abraham <josuea@noukod.com>
2019-01-13 08:38:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,9 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
-   get '/' do 
-    "Hello, world!"
+
+  get '/' do
+    "Hello, World!"
   end
 
 end
